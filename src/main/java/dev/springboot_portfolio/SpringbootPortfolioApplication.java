071e8791fb029680a5a7e154ed729d550e92aee0
Lee Seunghwan <knetdev@knetbiz.com>
2019-08-12 08:39:40 +0900

    20190812_최초커밋_스프링부트프로젝트 생성 및 초기셋팅

---
+++ src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
@@ -0,0 +1,13 @@
+package dev.springboot_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPortfolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/springboot_portfolio/SpringbootPortfolioApplicationTests.java
@@ -0,0 +1,16 @@
+package dev.springboot_portfolio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootPortfolioApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
