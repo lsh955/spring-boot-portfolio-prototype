cfb89b730ea2882146a34e8e01ee6b9b00e3d023
Lee Seunghwan <knetdev@knetbiz.com>
2019-08-26 13:03:50 +0900

    mariaDB implementation 추가및 계정정보 추가

--- src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
+++ src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 // DB연결을 하지않을 때 아래의 어노테이션을 적용한다.
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+// @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SpringbootPortfolioApplication {
 
     public static void main(String[] args) {
