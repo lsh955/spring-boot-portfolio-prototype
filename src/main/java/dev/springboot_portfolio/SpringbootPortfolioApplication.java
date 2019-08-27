e639086b89b7cee7b9295fb3a63864d25eb532d3
이승환 <lshk955@naver.com>
2019-08-27 13:51:04 +0900

    basePackages 작성

--- src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
+++ src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 // DB연결을 하지않을 때 아래의 어노테이션을 적용한다.
-// @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+// @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}
 
 @MapperScan(basePackages = "dev.springboot_portfolio")
 public class SpringbootPortfolioApplication {
