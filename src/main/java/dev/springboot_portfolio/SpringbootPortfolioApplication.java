aae8624103f72cf6d38aebc090e543645d73afa0
이승환 <lshk955@naver.com>
2019-08-27 15:42:28 +0900

    Spring Boot security 추가및 Spring Boot에서 기본 보안 비밀번호 사용 해제를 위한 조치

--- src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
+++ src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
@@ -3,11 +3,13 @@ package dev.springboot_portfolio;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+import javax.annotation.security.PermitAll;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 // DB연결을 하지않을 때 아래의 어노테이션을 적용한다.
 // @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}
-
 @MapperScan(basePackages = "dev.springboot_portfolio")
 public class SpringbootPortfolioApplication {
 
