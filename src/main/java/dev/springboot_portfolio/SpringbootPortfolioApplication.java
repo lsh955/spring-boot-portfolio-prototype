4f3ed49c1c99b68a4cd8d26700897952705d4673
이승환 <lshk955@naver.com>
2019-08-27 15:43:10 +0900

    어노테이션 순서변경

--- src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
+++ src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
@@ -5,12 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-import javax.annotation.security.PermitAll;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 // DB연결을 하지않을 때 아래의 어노테이션을 적용한다.
 // @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}
 @MapperScan(basePackages = "dev.springboot_portfolio")
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class SpringbootPortfolioApplication {
 
     public static void main(String[] args) {
