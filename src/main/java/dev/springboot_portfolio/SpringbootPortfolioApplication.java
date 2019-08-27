0a1bd39a514d93231b43787b03d5bbd335834fd6
이승환 <lshk955@naver.com>
2019-08-27 13:44:29 +0900

    mapper 추가에 따른 작업

--- src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
+++ src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
@@ -1,13 +1,14 @@
 package dev.springboot_portfolio;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 // DB연결을 하지않을 때 아래의 어노테이션을 적용한다.
-//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+// @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+
+@MapperScan(basePackages = "dev.springboot_portfolio")
 public class SpringbootPortfolioApplication {
 
     public static void main(String[] args) {
