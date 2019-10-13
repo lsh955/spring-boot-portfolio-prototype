dff1fc77e478f74936039888534e33f5d1808d0e
Lee SeungHwan <lsh01087674117@gmail.com>
2019-10-13 11:31:41 +0900

    라인변경, 텍스트변, db계정변경

--- src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
+++ src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
@@ -2,13 +2,15 @@ package dev.springboot_portfolio;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-// DB연결을 하지않을 때 아래의 어노테이션을 적용한다.
-// @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}
 @MapperScan(basePackages = "dev.springboot_portfolio")
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+// DB연결을 하지않을 때 아래의 어노테이션을 적용한다.
+//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}
 public class SpringbootPortfolioApplication {
 
     public static void main(String[] args) {
