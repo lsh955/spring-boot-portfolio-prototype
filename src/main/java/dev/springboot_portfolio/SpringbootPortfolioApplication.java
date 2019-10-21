4675a8d7832ae3554d289676ca79d160ccd451be
이승환 <knetdev@knetbiz.com>
2019-10-21 11:45:00 +0900

    빌드부분 수정

--- src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
+++ src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
@@ -7,15 +7,21 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @MapperScan(basePackages = "dev.springboot_portfolio")
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 // DB연결을 하지않을 때 아래의 어노테이션을 적용한다.
 //@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}
-public class SpringbootPortfolioApplication {
+public class SpringbootPortfolioApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringbootPortfolioApplication.class, args);
+//    public static void main(String[] args) {
+//        SpringApplication.run(SpringbootPortfolioApplication.class, args);
+//    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootPortfolioApplication.class);
     }
-
 }
