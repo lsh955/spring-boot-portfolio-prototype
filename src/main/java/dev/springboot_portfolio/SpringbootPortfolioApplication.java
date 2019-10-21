52c782007e313fe63dc986f144315dae94a0fc75
이승환 <knetdev@knetbiz.com>
2019-10-21 16:41:21 +0900

    불필요한 코드 삭제

--- src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
+++ src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
@@ -1,16 +1,13 @@
 package dev.springboot_portfolio;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-//@MapperScan(basePackages = "dev.springboot_portfolio")
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 // DB연결을 하지않을 때 아래의 어노테이션을 적용한다.
 //@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}
@@ -23,7 +20,6 @@ public class SpringbootPortfolioApplication extends SpringBootServletInitializer
     
     public static void main(String[] args) {
         SpringApplication.run(SpringbootPortfolioApplication.class, args);
-        System.out.println("시작");
     }
 
 }
