e7c6071a7490d70e3125f4d0d644a704a8e61923
이승환 <knetdev@knetbiz.com>
2019-10-21 10:32:13 +0900

    추가

--- src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
+++ src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
@@ -1,5 +1,6 @@
 package dev.springboot_portfolio;
 
+import jdk.tools.jaotc.Main;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -12,6 +13,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 // DB연결을 하지않을 때 아래의 어노테이션을 적용한다.
 //@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}
 public class SpringbootPortfolioApplication {
+    
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootPortfolioApplication.class, args);
