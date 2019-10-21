2af9d09059ba25c5335f3495e4389bfbf50fbc46
이승환 <knetdev@knetbiz.com>
2019-10-21 13:16:23 +0900

    리펙토링

--- src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
+++ src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
@@ -1,7 +1,5 @@
 package dev.springboot_portfolio;
 
-import jdk.tools.jaotc.Main;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,19 +7,23 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@MapperScan(basePackages = "dev.springboot_portfolio")
+@EnableJpaAuditing
+//@MapperScan(basePackages = "dev.springboot_portfolio")
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 // DB연결을 하지않을 때 아래의 어노테이션을 적용한다.
 //@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}
 public class SpringbootPortfolioApplication extends SpringBootServletInitializer {
 
-//    public static void main(String[] args) {
-//        SpringApplication.run(SpringbootPortfolioApplication.class, args);
-//    }
-    
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
         return application.sources(SpringbootPortfolioApplication.class);
     }
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPortfolioApplication.class, args);
+        System.out.println("시작");
+    }
+
 }
