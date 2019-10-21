a8be101fcea9e7d583e270870415535fc9167f1d
이승환 <knetdev@knetbiz.com>
2019-10-21 10:38:29 +0900

    dependencies 부분 변경

--- src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
+++ src/main/java/dev/springboot_portfolio/SpringbootPortfolioApplication.java
@@ -13,7 +13,6 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 // DB연결을 하지않을 때 아래의 어노테이션을 적용한다.
 //@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}
 public class SpringbootPortfolioApplication {
-    
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootPortfolioApplication.class, args);
