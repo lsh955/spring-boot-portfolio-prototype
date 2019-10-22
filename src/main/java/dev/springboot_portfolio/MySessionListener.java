7cd8c6cdf39f517828328a01c1c641aea708e329
이승환 <knetdev@knetbiz.com>
2019-10-22 16:35:55 +0900

    리펙토링

---
+++ src/main/java/dev/springboot_portfolio/MySessionListener.java
@@ -0,0 +1,21 @@
+package dev.springboot_portfolio;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+public class MySessionListener implements HttpSessionListener {
+    
+    @Override
+    public void sessionCreated(HttpSessionEvent event) {
+        // 세션이 생성될때
+        HttpSessionListener.super.sessionCreated(event);
+    }
+    
+    @Override
+    public void sessionDestroyed(HttpSessionEvent event) {
+        // 세션이 없어질때
+        HttpSession session = event.getSession();
+    }
+
+}
