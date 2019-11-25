7f57a34fd61e27aaa2d8ea91eb37f87be985294e
이승환 <knetdev@knetbiz.com>
2019-11-25 20:19:21 +0900

    테스트 코드

---
+++ src/main/java/vo/Test01.java
@@ -0,0 +1,14 @@
+package vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Test01 {
+
+    private String name;
+    private String age;
+    private String tel;
+
+}
