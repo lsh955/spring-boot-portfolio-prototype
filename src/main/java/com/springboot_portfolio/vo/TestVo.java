e5295d3fba514dea5c04c6020b78928cb4a4575a
이승환 <knetdev@knetbiz.com>
2019-11-26 13:46:31 +0900

    lombok 어노테이션 제거

--- src/main/java/com/springboot_portfolio/vo/TestVo.java
+++ src/main/java/com/springboot_portfolio/vo/TestVo.java
@@ -1,18 +1,49 @@
 package com.springboot_portfolio.vo;
 
-import lombok.Getter;
-import lombok.Setter;
-
 /**
  * @author 이승환
  * @since 2019-11-26
  */
-@Getter
-@Setter
 public class TestVo {
     
+    private int idx;
     private String name;
     private String age;
     private String tel;
+    
+    public int getIdx() {
+        return idx;
+    }
+    
+    public void setIdx(int idx) {
+        this.idx = idx;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getAge() {
+        return age;
+    }
+    
+    public void setAge(String age) {
+        this.age = age;
+    }
+    
+    public String getTel() {
+        return tel;
+    }
+    
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+    
+   
+    
     
 }
