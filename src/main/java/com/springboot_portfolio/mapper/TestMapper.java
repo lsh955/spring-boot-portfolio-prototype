2fc7dec1439aba7707e4ca2b2491c70700a9e70c
이승환 <knetdev@knetbiz.com>
2019-11-26 10:24:20 +0900

    주석입력

---
+++ src/main/java/com/springboot_portfolio/mapper/TestMapper.java
@@ -0,0 +1,11 @@
+package com.springboot_portfolio.mapper;
+
+import com.springboot_portfolio.vo.Test01;
+import java.util.List;
+
+public interface TestMapper {
+    
+    // 메소드이름은 Test-mapper.xml 파일의 select태그 id
+    public List<Test01> getAll() throws Exception;
+    
+}
