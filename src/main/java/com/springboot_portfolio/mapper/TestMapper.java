71cb9e1aac08e9dbe3f5e1e74732907ac0bd2641
이승환 <knetdev@knetbiz.com>
2019-11-27 09:46:41 +0900

    Mapper 인터페이스에 Mapper 어노테이션 추가 및 Mapper xml 주석입력및 코드정렬

--- src/main/java/com/springboot_portfolio/mapper/TestMapper.java
+++ src/main/java/com/springboot_portfolio/mapper/TestMapper.java
@@ -1,6 +1,7 @@
 package com.springboot_portfolio.mapper;
 
 import com.springboot_portfolio.vo.TestVo;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
@@ -8,6 +9,7 @@ import java.util.List;
  * @author 이승환
  * @since 2019-11-26
  */
+@Mapper
 public interface TestMapper {
     
     // 메소드이름은 Test-mapper.xml 파일의 select태그 id
