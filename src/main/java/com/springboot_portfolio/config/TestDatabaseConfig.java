b05854fe1643525836e71022bd0132e1aefa141b
이승환 <knetdev@knetbiz.com>
2019-11-27 09:28:39 +0900

    안쓰는 import 삭제

--- src/main/java/com/springboot_portfolio/config/TestDatabaseConfig.java
+++ src/main/java/com/springboot_portfolio/config/TestDatabaseConfig.java
@@ -4,7 +4,6 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
