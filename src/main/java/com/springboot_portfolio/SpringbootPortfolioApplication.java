3fa7b251f67fb554c1ac8a767427bed0f4daa338
이승환 <knetdev@knetbiz.com>
2019-12-05 17:46:27 +0900

    JPA방식이라 Mybatis방식을 위해 지우고 다시시작.

--- src/main/java/com/springboot_portfolio/SpringbootPortfolioApplication.java
+++ src/main/java/com/springboot_portfolio/SpringbootPortfolioApplication.java
@@ -2,6 +2,7 @@ package com.springboot_portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
@@ -9,10 +10,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  * @since 2019-11-26
  */
 @EnableJpaAuditing
-// @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 // DB연결을 하지않을 때 아래의 어노테이션을 적용한다.
 // @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}
-@SpringBootApplication
 public class SpringbootPortfolioApplication{
     
     public static void main(String[] args) {
