d618548d17f418fa20d17a7ea444b2c353548fc1
Lee SeungHwan <lsh01087674117@gmail.com>
2019-12-07 16:25:05 +0900

    (exclude = {SecurityAutoConfiguration.class}) 부분삭제

--- src/main/java/com/springboot_portfolio/SpringbootPortfolioApplication.java
+++ src/main/java/com/springboot_portfolio/SpringbootPortfolioApplication.java
@@ -10,7 +10,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  * @since 2019-11-26
  */
 @EnableJpaAuditing
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
+//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 // DB연결을 하지않을 때 아래의 어노테이션을 적용한다.
 // @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}
 public class SpringbootPortfolioApplication{
