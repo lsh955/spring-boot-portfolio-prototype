12653c5319d3dd6d8663e9d608c043a095d28571
Lee SeungHwan <lsh01087674117@gmail.com>
2019-12-07 17:07:43 +0900

    SpringApplicationBuilder 클래스에 SpingApplicationBuilder 추가

--- src/main/java/com/springboot_portfolio/SpringbootPortfolioApplication.java
+++ src/main/java/com/springboot_portfolio/SpringbootPortfolioApplication.java
@@ -2,6 +2,8 @@ package com.springboot_portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
@@ -13,8 +15,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 // @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 // DB연결을 하지않을 때 아래의 어노테이션을 적용한다.
 // @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}
-public class SpringbootPortfolioApplication{
-    
+public class SpringbootPortfolioApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
+        return application.sources(SpringbootPortfolioApplication.class);
+    }
+
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootPortfolioApplication.class, args);
     }
