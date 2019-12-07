09621b15f6882027f7fc94361bd51fe77ae9d657
Lee SeungHwan <lsh01087674117@gmail.com>
2019-12-07 16:25:56 +0900

    주석수정

--- src/main/java/com/springboot_portfolio/SpringbootPortfolioApplication.java
+++ src/main/java/com/springboot_portfolio/SpringbootPortfolioApplication.java
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  */
 @EnableJpaAuditing
 @SpringBootApplication
-//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+// @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 // DB연결을 하지않을 때 아래의 어노테이션을 적용한다.
 // @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}
 public class SpringbootPortfolioApplication{
