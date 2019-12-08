bc139b5aa7a864609d3a9394e9463dd4d775b232
Lee SeungHwan <lsh01087674117@gmail.com>
2019-12-08 14:10:09 +0900

    UserController class 작성

---
+++ src/main/java/com/springboot_portfolio/controller/UserController.java
@@ -0,0 +1,80 @@
+package com.springboot_portfolio.controller;
+
+import com.springboot_portfolio.domain.UserPrincipal;
+import com.springboot_portfolio.service.UserService;
+import com.springboot_portfolio.vo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+
+/**
+ * @author 이승환
+ * @since 2019/12/08
+ */
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(value = {"/", "login"})
+    public ModelAndView getLoginPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("user/login");
+        return modelAndView;
+    }
+
+    @GetMapping("registration")
+    public ModelAndView getRegistrationPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User();
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("user/registration");
+        return modelAndView;
+    }
+
+    @PostMapping("registration")
+    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+        User userExists = userService.findUserByLoginId(user.getLoginId());
+        if (userExists != null) {
+            bindingResult.rejectValue("loginId", "error.loginId", "There is already a user registered with the loginId provided");
+        }
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("user/registration");
+        } else {
+            userService.saveUser(user);
+            modelAndView.addObject("successMessage", "User has been registered successfully");
+            modelAndView.addObject("user", new User());
+            modelAndView.setViewName("user/registration");
+        }
+        return modelAndView;
+    }
+
+    @GetMapping("home")
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        UserPrincipal userPrincipal = (UserPrincipal) auth.getPrincipal();
+        System.out.println(userPrincipal.toString());
+        modelAndView.addObject("userName", "Welcome " + userPrincipal.getName() + " (" + userPrincipal.getId() + ")");
+        modelAndView.addObject("adminMessage", "Content Available Only for Users with Admin Role");
+        modelAndView.setViewName("user/home");
+        return modelAndView;
+    }
+
+    @GetMapping("exception")
+    public ModelAndView getUserPermissionExceptionPage() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("user/access-denied");
+        return mv;
+    }
+
+}
