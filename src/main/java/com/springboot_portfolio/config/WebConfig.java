6851027eeca7ae33fdea51988132af2e03cff87c
이승환 <knetdev@knetbiz.com>
2019-12-10 16:12:02 +0900

    필요없는 import라인 삭제

--- src/main/java/com/springboot_portfolio/config/WebConfig.java
+++ src/main/java/com/springboot_portfolio/config/WebConfig.java
@@ -3,7 +3,6 @@ package com.springboot_portfolio.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
