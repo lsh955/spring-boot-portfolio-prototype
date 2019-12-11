2a5ea9a3c19124d78317005fcf25d1eb5f2b65e0
이승환 <knetdev@knetbiz.com>
2019-12-11 15:09:22 +0900

    주석입력

--- src/main/java/com/springboot_portfolio/domain/UserGrant.java
+++ src/main/java/com/springboot_portfolio/domain/UserGrant.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.GrantedAuthority;
  * @author 이승환
  * @since 2019/12/08
  */
-public class UserGrant implements GrantedAuthority {
+public class UserGrant implements GrantedAuthority {    // GrantedAuthority : Spring Security의 개념과 구현은 액세스 권한을 부여 / 제어하는​권한을 얻는다.
     
     @Override
     public String getAuthority() {
