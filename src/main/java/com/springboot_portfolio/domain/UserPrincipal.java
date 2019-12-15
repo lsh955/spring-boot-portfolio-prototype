1cdc1bb479f631b62890e5060e8e970239f3d01a
Lee SeungHwan <lsh01087674117@gmail.com>
2019-12-15 21:58:11 +0900

    주석입력, 리펙토링

--- src/main/java/com/springboot_portfolio/domain/UserPrincipal.java
+++ src/main/java/com/springboot_portfolio/domain/UserPrincipal.java
@@ -6,10 +6,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author 이승환
@@ -17,7 +14,7 @@ import java.util.List;
  */
 @ToString
 public class UserPrincipal implements UserDetails {
-    
+
     private User user;
 
     public void setUser(User user) {
@@ -39,32 +36,32 @@ public class UserPrincipal implements UserDetails {
     public String getName() {
         return user.getUserName();
     }
-    
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {    //유저가 갖고 있는 권한 목록
-        return Arrays.asList(new UserGrant());
+        return Collections.singletonList(new UserGrant());
     }
-    
+
     @Override
     public String getPassword() {   //유저 비밀번호
         return user.getPassword();
     }
-    
+
     @Override
     public String getUsername() {   // 유저 이름 혹은 아이디
         return user.getUserName();
     }
-    
+
     @Override
     public boolean isAccountNonExpired() {  // 유저 아이디가 만료 되었는지
         return true;
     }
-    
+
     @Override
     public boolean isAccountNonLocked() {   // 유저 아이디가 Lock 걸렸는지
         return true;
     }
-    
+
     @Override
     public boolean isCredentialsNonExpired() {  //비밀번호가 만료 되었는지
         return true;
@@ -72,7 +69,7 @@ public class UserPrincipal implements UserDetails {
 
     @Override
     public boolean isEnabled() {    // 계정이 활성화 되었는지
-    //  return user.getActive() == 1;
+        //  return user.getActive() == 1;
         return true;
     }
 
