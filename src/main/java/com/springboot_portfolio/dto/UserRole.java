38a3b8a9df3e64b03dbade54cf99da11a34fa69b
이승환 <knetdev@knetbiz.com>
2019-12-19 08:51:33 +0900

    객체class에 Component 어노테이션 작성

--- src/main/java/com/springboot_portfolio/dto/UserRole.java
+++ src/main/java/com/springboot_portfolio/dto/UserRole.java
@@ -1,11 +1,14 @@
 package com.springboot_portfolio.dto;
 
+import org.springframework.stereotype.Component;
+
 /**
  * @author 이승환
  * @since 2019/12/08
  *
  * Input, Output 클래스 모음. domain 클래스와 비슷할 수 있지만, View 와 인터페이스 하기 위한 클래스.
  */
+@Component
 public class UserRole {
 
     private int userId;
