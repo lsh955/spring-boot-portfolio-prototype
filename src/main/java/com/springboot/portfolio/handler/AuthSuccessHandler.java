dd9d911a0ee0e78223edcc8df1871383384fa994
이승환 <knetdev@knetbiz.com>
2019-12-27 09:14:09 +0900

    리펙터링

--- src/main/java/com/springboot/portfolio/handler/AuthSuccessHandler.java
+++ src/main/java/com/springboot/portfolio/handler/AuthSuccessHandler.java
@@ -18,36 +18,10 @@ import java.io.IOException;
 @Configuration
 public class AuthSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
     
-    private static final String Unknown = "unknown";
-    
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
         response.setStatus(HttpServletResponse.SC_OK);  // 요청이 정상적으로 완료되었음을 나타내는 상태 코드 (200).
         response.sendRedirect("/home");         // 성공 후 이동할 페이지를 지정
-    }
-    
-    /**
-     * 클라이언트 IP정보
-     */
-    public static String getClientIp(HttpServletRequest request){
-        String ip = request.getHeader("X-Forwarded-For");
-    
-        if (ip == null || ip.length() == 0 || Unknown.equalsIgnoreCase(ip)) {
-            ip = request.getHeader("Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || Unknown.equalsIgnoreCase(ip)) {
-            ip = request.getHeader("WL-Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || Unknown.equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_CLIENT_IP");
-        }
-        if (ip == null || ip.length() == 0 || Unknown.equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-        }
-        if (ip == null || ip.length() == 0 || Unknown.equalsIgnoreCase(ip)) {
-            ip = request.getRemoteAddr();
-        }
-        return ip;
     }
     
 }
