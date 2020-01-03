bb5e96ca233ceac0b4888260443f01e08698c2b4
이승환 <knetdev@knetbiz.com>
2020-01-03 14:31:26 +0900

    메인 테스트코드에 데이터베이스 연결유무 코드작성

--- src/test/java/com/springboot/portfolio/SpringbootPortfolioApplicationTests.java
+++ src/test/java/com/springboot/portfolio/SpringbootPortfolioApplicationTests.java
@@ -3,12 +3,26 @@ package com.springboot.portfolio;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 @SpringBootTest
 public class SpringbootPortfolioApplicationTests {
     
-    @Test
-    public void contextLoads() {
+    private static final String DRIVER = "org.mariadb.jdbc.Driver";
+    private static final String URL = "jdbc:mariadb://codedot.co.kr:3306/test_user_01?characterEncoding=UTF-8";
+    private static final String USER = "test_user_01";
+    private static final String PW = "dl945pa";
     
+    @Test
+    public void contextLoads() throws Exception {
+        System.out.println(USER + " : DataBase Connect Start...");
+        Class.forName(DRIVER);
+        try (Connection getcon = DriverManager.getConnection(URL, USER, PW)) {
+            System.out.println(USER + " : DataBase Connect Success");
+        } catch (Exception e) {
+            System.err.println(USER + " : DataBase Connect Failure");
+        }
     }
     
 }
