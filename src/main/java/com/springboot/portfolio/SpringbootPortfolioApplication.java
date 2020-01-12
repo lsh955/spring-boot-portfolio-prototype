696f6ec53eb8631758c70848710f83f220e6552f
Lee SeungHwan <lsh01087674117@gmail.com>
2020-01-12 10:28:18 +0900

    안쓰는 import 제거

--- src/main/java/com/springboot/portfolio/SpringbootPortfolioApplication.java
+++ src/main/java/com/springboot/portfolio/SpringbootPortfolioApplication.java
@@ -5,20 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import java.net.InetAddress;
+
 /**
  * @author 이승환
  * @since 2019-11-26
  */
 @SpringBootApplication
 public class SpringbootPortfolioApplication extends SpringBootServletInitializer {
-    
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(SpringbootPortfolioApplication.class);   // 어플리케이션을 WAR 형태로 배포하기위해 아래와 같은 메인코드를 작성한다.
     }
-    
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootPortfolioApplication.class, args);
     }
-    
 }
