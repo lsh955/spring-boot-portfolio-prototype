8d0ce6578261ea9d4b28ea8d452fc3bbf4f546e2
이승환 <knetdev@knetbiz.com>
2020-01-15 09:35:25 +0900

    enum(열거형)에 대한 주석 추가

--- src/main/java/com/springboot/portfolio/enums/UserType.java
+++ src/main/java/com/springboot/portfolio/enums/UserType.java
@@ -3,6 +3,11 @@ package com.springboot.portfolio.enums;
 /**
  * @author 이승환
  * @since 2020-01-10
+ *
+ * enum(열거형)?
+ * - 클래스처럼 보이게 하는 상수
+ * - 서로 관련있는 상수들끼리 모아 상수들을 대표할 수 있는 이름으로 타입을 정의하는 것
+ * - Enum 클래스 형을 기반으로 한 클래스형 선언
  */
 public enum UserType {
     
