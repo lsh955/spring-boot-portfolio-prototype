35f3dc1f66f77761c36ecdb24e624cd27934cdcc
Lee SeungHwan <lsh01087674117@gmail.com>
2020-01-19 17:01:38 +0900

    전체주석처리

--- src/main/java/com/springboot/portfolio/service/FCMService.java
+++ src/main/java/com/springboot/portfolio/service/FCMService.java
@@ -1,22 +1,22 @@
-package com.springboot.portfolio.service;
-
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.WebpushConfig;
-import com.google.firebase.messaging.WebpushNotification;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import sun.plugin2.message.Message;
-
-import java.util.concurrent.ExecutionException;
-
-/**
- * @author 이승환
- * @since 2020/01/19
- */
-@Slf4j
-@Service
-public class FCMService {
-
+//package com.springboot.portfolio.service;
+//
+//import com.google.firebase.messaging.FirebaseMessaging;
+//import com.google.firebase.messaging.WebpushConfig;
+//import com.google.firebase.messaging.WebpushNotification;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Service;
+//import sun.plugin2.message.Message;
+//
+//import java.util.concurrent.ExecutionException;
+//
+///**
+// * @author 이승환
+// * @since 2020/01/19
+// */
+//@Slf4j
+//@Service
+//public class FCMService {
+//
 //    public void send(final NotificationRequest notificationRequest) throws InterruptedException, ExecutionException {
 //        Message message = Message.builder()
 //                .setToken(notificationRequest.getToken())
@@ -29,5 +29,5 @@ public class FCMService {
 //        String response = FirebaseMessaging.getInstance().sendAsync(message).get();
 //        log.info("Sent message: " + response);
 //    }
-
-}
+//
+//}
