1b9efdbcf2a16d23ac6b8d69b33cd4c1adcc7d8b
Lee SeungHwan <lsh01087674117@gmail.com>
2020-01-24 18:28:22 +0900

    정렬

--- src/main/java/com/springboot/portfolio/config/SecurityConfiguration.java
+++ src/main/java/com/springboot/portfolio/config/SecurityConfiguration.java
@@ -68,7 +68,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .failureHandler(authFailureHandler);                                    // 로그인이 실패했을 때 핸들러
 
         http.logout()                                                                   // 로그아웃 처리
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))     // 로그아웃이 성공했을 경우 이동할 페이지
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))      // 로그아웃이 성공했을 경우 이동할 페이지
                 .logoutSuccessUrl("/?state=logout")                                     // 로그아웃 성공 후 반환하는 URI
                 .invalidateHttpSession(true);                                           // 로그아웃시 인증정보를 지우하고 세션을 무효화 시킨다는 설정
 
