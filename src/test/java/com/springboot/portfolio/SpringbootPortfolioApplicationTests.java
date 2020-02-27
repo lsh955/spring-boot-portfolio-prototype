3a1eeec850aa5bbddad614b971697aadfdb832ee
이승환 <lshk955@naver.com>
2020-02-27 14:35:29 +0900

    원래대로..

--- src/test/java/com/springboot/portfolio/SpringbootPortfolioApplicationTests.java
+++ src/test/java/com/springboot/portfolio/SpringbootPortfolioApplicationTests.java
@@ -9,20 +9,20 @@ import java.sql.DriverManager;
 @SpringBootTest
 public class SpringbootPortfolioApplicationTests {
 
-//    private static final String DRIVER = "org.mariadb.jdbc.Driver";
-//    private static final String URL = "jdbc:mariadb://codedot.co.kr:3306/test_user_01?characterEncoding=UTF-8";
-//    private static final String USER = "test_user_01";
-//    private static final String PW = "dl945pa";
+    private static final String DRIVER = "org.mariadb.jdbc.Driver";
+    private static final String URL = "jdbc:mariadb://codedot.co.kr:3306/test_user_01?characterEncoding=UTF-8";
+    private static final String USER = "test_user_01";
+    private static final String PW = "dl945pa";
 
-//    @Test
-//    public void contextLoads() throws Exception {
-//        System.out.println(USER + " : DataBase Connect Start...");
-//        Class.forName(DRIVER);
-//        try (Connection getcon = DriverManager.getConnection(URL, USER, PW)) {
-//            System.out.println(USER + " : DataBase Connect Success");
-//        } catch (Exception e) {
-//            System.err.println(USER + " : DataBase Connect Failure");
-//        }
-//    }
+    @Test
+    public void contextLoads() throws Exception {
+        System.out.println(USER + " : DataBase Connect Start...");
+        Class.forName(DRIVER);
+        try (Connection getcon = DriverManager.getConnection(URL, USER, PW)) {
+            System.out.println(USER + " : DataBase Connect Success");
+        } catch (Exception e) {
+            System.err.println(USER + " : DataBase Connect Failure");
+        }
+    }
 
 }
