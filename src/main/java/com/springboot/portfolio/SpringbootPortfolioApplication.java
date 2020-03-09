1a3f9e4be1fb2b71d5c362125874d0d59a668456
이승환 <lshk955@naver.com>
2020-03-10 08:34:48 +0900

    리펙터링

--- src/main/java/com/springboot/portfolio/SpringbootPortfolioApplication.java
+++ src/main/java/com/springboot/portfolio/SpringbootPortfolioApplication.java
@@ -16,7 +16,7 @@ public class SpringbootPortfolioApplication extends SpringBootServletInitializer
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(SpringbootPortfolioApplication.class);   // 어플리케이션을 WAR 형태로 배포하기위해 아래와 같은 메인코드를 작성한다.
     }
-
+    
     public static void main(String[] args) {
         SpringApplication.run(SpringbootPortfolioApplication.class, args);
     }
