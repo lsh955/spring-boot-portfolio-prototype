4b6fd4b6db81b49b5d2958b985650322b94a4e9a
이승환 <lshk955@naver.com>
2020-03-10 17:44:24 +0900

    mariadb 으로 다시 복구

--- src/test/java/com/springboot/portfolio/SpringbootPortfolioApplicationTests.java
+++ src/test/java/com/springboot/portfolio/SpringbootPortfolioApplicationTests.java
@@ -9,20 +9,20 @@ import java.sql.DriverManager;
 @SpringBootTest
 public class SpringbootPortfolioApplicationTests {
     
-//    private static final String DRIVER = "org.mysql.jdbc.Driver";
-//    private static final String URL = "jdbc:mysql://codedot.co.kr:3306/test_user_01?characterEncoding=UTF-8";
-//    private static final String USER = "test_user_01";
-//    private static final String PW = "dl945pa";
-//
-//    @Test
-//    public void contextLoads() throws Exception {
-//        System.out.println(USER + " : 테스트 데이터베이스 연결중...");
-//        Class.forName(DRIVER);
-//        try (Connection getcon = DriverManager.getConnection(URL, USER, PW)) {
-//            System.out.println(USER + " : 테스트 데이터베이스 연결성공");
-//        } catch (Exception e) {
-//            System.err.println(USER + " : 테스트 데이터베이스 연결실패");
-//        }
-//    }
+    private static final String DRIVER = "org.mariadb.jdbc.Driver";
+    private static final String URL = "jdbc:mariadb://codedot.co.kr:3306/test_user_01?characterEncoding=UTF-8";
+    private static final String USER = "test_user_01";
+    private static final String PW = "dl945pa";
+    
+    @Test
+    public void contextLoads() throws Exception {
+        System.out.println(USER + " : 테스트 데이터베이스 연결중...");
+        Class.forName(DRIVER);
+        try (Connection getcon = DriverManager.getConnection(URL, USER, PW)) {
+            System.out.println(USER + " : 테스트 데이터베이스 연결성공");
+        } catch (Exception e) {
+            System.err.println(USER + " : 테스트 데이터베이스 연결실패");
+        }
+    }
     
 }
