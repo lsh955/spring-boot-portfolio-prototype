02db3f0524d977c1db8f6f2e341f69bd30f7e379
이승환 <lsh01087674117@gmail.com>
2020-07-21 13:37:39 +0900

    UserController 리펙토링

--- src/main/java/com/portfolio/controller/UserController.java
+++ src/main/java/com/portfolio/controller/UserController.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,8 +23,8 @@ import javax.validation.Valid;
  * 컨트롤러 클래스 모음.
  */
 @Slf4j
-@RequiredArgsConstructor
 @Controller
+@RequiredArgsConstructor
 public class UserController {
 
 	private final SignUpService signUpService;
@@ -35,16 +34,12 @@ public class UserController {
 	 */
 	@GetMapping({"/", "/index"})
 	public String getIndex(HttpServletRequest request) {
-
 		String header = request.getHeader("User-Agent");
-
-		// IE환경 검증
-		if (header.indexOf("MSIE") > -1 || header.indexOf("Trident") > -1) {
+		if (header.indexOf("MSIE") > -1 || header.indexOf("Trident") > -1) {	// IE환경 검증
 			return "browser_issue"; // 브라우저 업그레이드 권장페이지
 		} else {
 			return "index"; // 메인페이지
 		}
-
 	}
 
 	/**
@@ -52,12 +47,9 @@ public class UserController {
 	 */
 	@GetMapping("signup")
 	public String getSignUpPage(Model model) {
-
 		UserDao userDao = new UserDao();                        // 회원 데이터
 		model.addAttribute("userDao", userDao);    // 뷰로 보낼 데이터 값
-
 		return "signup";
-
 	}
 
 	/**
@@ -66,13 +58,12 @@ public class UserController {
 	 * @param request
 	 * @return
 	 */
-	@PostMapping("setsignup")
-	public String getSetSignUp(Model model, @Valid UserDao userDao, BindingResult bindingResult, HttpServletRequest request) {
-		model.addAttribute("user", new UserDao());
-		userDao.setUserIpAddress(request.getRemoteAddr());	// IP 주소
+	@PostMapping("sendsignup")
+	public String getSendSignUp(@Valid UserDao userDao, HttpServletRequest request) {
 		if (signUpService.SignUpIdCheck(userDao).equals("Overlap")) {
 			return "signup";	// 회원가입 실패(아이디중복)
 		}
+		userDao.setUserIpAddress(request.getRemoteAddr());	// 접속 IP Address
 		return "index";		// 회원가입 성공
 	}
 
@@ -95,20 +86,17 @@ public class UserController {
 	/**
 	 * 구글 리캡챠
 	 *
-	 * @param recaptchaResponse
+	 * @param reCaptcha
 	 * @param request
 	 * @return
 	 */
 	@PostMapping("reCAPTCHA")
-	public reCaptcha reCAPTCHA(@RequestParam(name = "g-recaptcha-response") String recaptchaResponse, HttpServletRequest request) {
+	public reCaptcha reCAPTCHA(@RequestParam(name = "g-recaptcha-response") String reCaptcha, HttpServletRequest request) {
 		String ip = request.getRemoteAddr();
 		String url = "https://www.google.com/recaptcha/api/siteverify";
-		String params = "?secret=6LfWFs8UAAAAAMng0MZUnuaYH83e5v6Jwv50Ci5T&response=" + recaptchaResponse;
-
+		String params = "?secret=6LfWFs8UAAAAAMng0MZUnuaYH83e5v6Jwv50Ci5T&response=" + reCaptcha;
 		RestTemplate restTemplate = new RestTemplate();
-
 		reCaptcha recaptcha = restTemplate.exchange(url + params, HttpMethod.POST, null, reCaptcha.class).getBody();
-
 		assert recaptcha != null;
 		if (recaptcha.isSuccess()) {
 			log.info("reCAPTCHA 성공");
