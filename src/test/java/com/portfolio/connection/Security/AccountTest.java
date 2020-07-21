4f0b16edafa7d4964a9c45db7b2377fbaf0655c7
이승환 <lsh01087674117@gmail.com>
2020-07-21 14:02:40 +0900

    AccountTest 추가, 리펙토링

---
+++ src/test/java/com/portfolio/connection/Security/AccountTest.java
@@ -0,0 +1,44 @@
+package com.portfolio.connection.Security;
+
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author 이승환
+ * @since 2020-07-21
+ */
+@DisplayName("Account All Test")
+@SpringBootTest
+public class AccountTest {
+
+	//TODO : 회원가입(실패)
+
+
+	//TODO : 회원가입(성공)
+
+
+	//TODO : 사용자권한 로그인(실패)
+
+
+	//TODO : 사용자권한 로그인(성공)
+	
+	
+	//TODO : 관리자권한 로그인(실패)
+
+
+	//TODO : 관리자권한 로그인(성공)
+
+
+	//TODO : 아이디찾기(실패)
+
+
+	//TODO : 아이디찾기(성공)
+
+
+	//TODO : 비밀번호찾기(실패)
+
+
+	//TODO : 비밀번호찾기(성공)
+
+
+}
