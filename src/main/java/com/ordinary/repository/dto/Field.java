882790cad76010750e85a32b879590e0f0f2e95b
이승환 <lsh01087674117@gmail.com>
2020-07-22 11:07:23 +0900

    SlackBot 리펙토리

---
+++ src/main/java/com/ordinary/repository/dto/Field.java
@@ -0,0 +1,17 @@
+package com.ordinary.repository.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author 이승환
+ * @since 2020-07-22
+ */
+@Getter
+@NoArgsConstructor
+public class Field {
+
+	private String title;
+	private String value;
+
+}
--- src/main/java/com/ordinary/service/SlackBotService.java
+++ src/main/java/com/ordinary/service/SlackBotService.java
@@ -1,8 +1,38 @@
 package com.ordinary.service;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ordinary.enums.SlackTarget;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
 /**
  * @author 이승환
  * @since 2020-07-22
  */
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class SlackBotService {
+
+	private final ObjectMapper objectMapper;
+
+	private String writeValueAsString(Object obj) {
+		try {
+			objectMapper.configure(JsonGenerator.Feature.ESCAPE_NON_ASCII, true);
+			return objectMapper.writeValueAsString(obj);
+		} catch (JsonProcessingException e) {
+			log.error("Occur JsonProcessingException: {}", e);
+			throw new IllegalArgumentException(e.getMessage());
+		}
+	}
+
+	public void send(SlackTarget target, Object object) {
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.postForEntity(target.getWebHookUrl(), writeValueAsString(object), String.class);
+	}
+
 }
